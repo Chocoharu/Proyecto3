e70a51f3698396254b1cd063ac5fcf3262532d22
Chocoharu <achocobarbel@gmail.com>
2023-07-19 08:26:39 -0400

    Correccion para clases

--- Proyecto3.cpp
+++ Proyecto3.cpp
@@ -189,7 +189,7 @@ void HeapSort(vector<int>& arr)
     }
     for (int i = n - 1; i > 0; --i)
 	{
-        swap(arr[0], arr[i]);
+        Swap(arr,arr[0], arr[i]);
         heapify(arr, i, 0);
     }
 }
